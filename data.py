14db52fd2564c3fdc33e8aab2e7838b0256d7970
mis <mohamediburahimsha.s@microfocus.com>
2019-12-09 16:46:09 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+
+    articles = [
+        {
+            'id': 1,
+            'title': "Articles One",
+            'body': "NetIQ Access Manager doesn't force you to trade business agility for better security controls. Ensure user productivity from any location by extending seamless access and security to all your users -- including partners. As a result, save time and money with heightened security and simplified administration. Access Manager is the only solution to combine advanced capabilities such as multi-factor authentication, data encryption, and Single Sign-on. NetIQ's comprehensive approach saves you time and money by eliminating siloed provisioning and access control systems and by automating manual, time consuming IT and business processes.",
+            'author': 'Mohamed S',
+            'create_date': '08-12-2019'
+        },
+        {
+            'id': 2,
+            'title': "Articles Two",
+            'body': "NetIQ Access Manager1 doesn't force you to trade business agility for better security controls. Ensure user productivity from any location by extending seamless access and security to all your users -- including partners. As a result, save time and money with heightened security and simplified administration. Access Manager is the only solution to combine advanced capabilities such as multi-factor authentication, data encryption, and Single Sign-on. NetIQ's comprehensive approach saves you time and money by eliminating siloed provisioning and access control systems and by automating manual, time consuming IT and business processes.",
+            'author': 'Iburahimsha S',
+            'create_date': '08-12-2019'
+        },
+        {
+            'id': 3,
+            'title': "Articles Three",
+            'body': "NetIQ Access Manager2 doesn't force you to trade business agility for better security controls. Ensure user productivity from any location by extending seamless access and security to all your users -- including partners. As a result, save time and money with heightened security and simplified administration. Access Manager is the only solution to combine advanced capabilities such as multi-factor authentication, data encryption, and Single Sign-on. NetIQ's comprehensive approach saves you time and money by eliminating siloed provisioning and access control systems and by automating manual, time consuming IT and business processes.",
+            'author': 'Aysha S',
+            'create_date': '08-12-2019'
+        }
+    ]
+    return articles
