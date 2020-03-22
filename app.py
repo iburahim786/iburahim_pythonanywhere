e9415df0f92a23cc975d18effe550c59d93ab037
mis <mohamediburahimsha.s@microfocus.com>
2020-03-22 23:27:08 +0530

    social authentication

--- app.py
+++ app.py
@@ -184,11 +184,14 @@ def load_user(user_id):
     return Users.query.get(int(user_id))
 
 
-twitter_blueprint = make_twitter_blueprint()
+twitter_blueprint = make_twitter_blueprint(api_key='d4wull0Yxr8j5dodoomZnERao',
+                                           api_secret='hvsoBEFSRbUrUb2w6FtlKuwg6SkpdtdT8CcNt4Oeo7v8u81sRN')
 
-github_blueprint = make_github_blueprint()
+github_blueprint = make_github_blueprint(client_id='Iv1.4c8e3b57fb85d327',
+                                         client_secret='e478405b19836c32524f2edc2f1f623c6be6b6ee')
 
-facebook_blueprint = make_facebook_blueprint()
+facebook_blueprint = make_facebook_blueprint(client_id='541542180110438',
+                                             client_secret='4622783c7c26da9e3059fbd2bf4ebc5f')
 
 app.register_blueprint(twitter_blueprint, url_prefix='/twitter_login')
 app.register_blueprint(github_blueprint, url_prefix='/github_login')
@@ -1652,4 +1655,4 @@ def delete_comment(comment_id, article_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context='adhoc')
+    app.run(debug=True)
